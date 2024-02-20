ded5accac1213048a43ccaaf5c46333652f4be0f
Towsif <towsif1528@gmail.com>
2024-02-20 23:17:41 +0600

    updated the lab task 4 scenario file

--- COMPUTER GRAPHICS/MID/LAB TASK/LAB 3/LAB3_TASK_4/main.cpp
+++ COMPUTER GRAPHICS/MID/LAB TASK/LAB 3/LAB3_TASK_4/main.cpp
@@ -8,6 +8,25 @@
 whenever the window needs to be re-painted. */
 
 
+void soil_effect(){
+
+glBegin(GL_POLYGON);
+glColor3f(0.8, 0.6, 0.4);
+glVertex2f(-194, -84);
+glVertex2f(-178, -68);
+glVertex2f(-184, -44);
+glVertex2f(-150, -24);
+glVertex2f(-58, -26);
+glVertex2f(164, -22);
+glVertex2f(205, -47);
+glVertex2f(302, -52);
+glVertex2f(325, -70);
+glVertex2f(330, -80);
+glVertex2f(334, -86);
+
+glEnd();
+}
+
 void tree(){
 
 glBegin(GL_POLYGON);
@@ -216,7 +235,7 @@ glVertex2f(80,-30);
 
 glEnd();
 
-//LOWER OUTLILNE UNDER THE RIGHT WINDOW\
+//LOWER OUTLILNE UNDER THE RIGHT WINDOW
 
 glBegin(GL_LINES);
 
@@ -478,6 +497,411 @@ glEnd();
 
 }
 
+void lampPost(){
+
+glColor3f(0, 0, 0.4);
+glBegin(GL_POLYGON);
+glVertex2f(183, -74);
+glVertex2f(180, -80);
+glVertex2f(200, -80);
+glVertex2f(197, -74);
+glEnd();
+
+glBegin(GL_POLYGON);
+glVertex2f(186, -71);
+glVertex2f(185, -74);
+glVertex2f(195, -74);
+glVertex2f(193.5, -71);
+
+glEnd();
+
+glBegin(GL_POLYGON);
+glVertex2f(187.5, 9.6);
+glVertex2f(185, 9.6);
+glVertex2f(182.9, 27.1);
+glVertex2f(196.6, 27.2);
+glVertex2f(194.2, 9.6);
+glVertex2f(191.8, 9.6);
+glVertex2f(191.625, -71);
+glVertex2f(187.5, -71);
+glEnd();
+
+glBegin(GL_POLYGON);
+
+glVertex2f(181.6, 28.6);
+glVertex2f(181.6, 27.2);
+glVertex2f(197.8, 27.2);
+glVertex2f(197.8, 28.6);
+
+glEnd();
+
+
+glBegin(GL_POLYGON);
+
+glVertex2f(182.5, 29.4);
+glVertex2f(182.5, 28.6);
+glVertex2f(196.8, 28.6);
+glVertex2f(196.8, 29.4);
+
+glEnd();
+glBegin(GL_POLYGON);
+
+glVertex2f(183.3, 29.4);
+glVertex2f(183.7, 34);
+glVertex2f(184.9, 36.1);
+glVertex2f(186.9, 37.5);
+glVertex2f(189, 37.8);
+glVertex2f(190.8, 37.7);
+glVertex2f(192.9, 36.5);
+glVertex2f(194.8, 34.4);
+glVertex2f(195.6, 31.6);
+glVertex2f(195.5701992480065, 29.4);
+
+glEnd();
+
+glBegin(GL_POLYGON);
+
+glVertex2f(188.499790461081, 37.730989124382);
+glVertex2f(188.5, 40);
+glVertex2f(189.5, 40);
+glVertex2f(189.5028454998969, 37.7720488899183);
+
+glEnd();
+
+glBegin(GL_POLYGON);
+glColor3f(1, 1, 0);
+glVertex2f(184.495930978, 27.1116491312282);
+glVertex2f(185, 20);
+glVertex2f(188.5, 20);
+glVertex2f(188.592557196, 27.1415515123092);
+
+glEnd();
+
+glBegin(GL_POLYGON);
+glColor3f(1, 1, 0);
+glVertex2f(190.6003196590304, 27.1562067128396);
+glVertex2f(190.5, 20);
+glVertex2f(194, 20);
+glVertex2f(194.8000958977091, 27.1868620138519);
+glEnd();
+
+glBegin(GL_POLYGON);
+glColor3f(1, 1, 0);
+glVertex2f(185.2, 18);
+glVertex2f(186, 11);
+glVertex2f(188.6, 11);
+glVertex2f(188.6, 18);
+
+glEnd();
+
+glBegin(GL_POLYGON);
+glColor3f(1, 1, 0);
+glVertex2f(190.3, 18);
+glVertex2f(190.2, 11);
+glVertex2f(193, 11);
+glVertex2f(194, 18);
+
+glEnd();
+
+
+}
+
+void bench(){
+
+//lower supports
+glBegin(GL_POLYGON);
+glColor3f(0.5, 0.3, 0);
+glVertex2f(212, -70);
+glVertex2f(212, -84);
+glVertex2f(215, -84);
+glVertex2f(215, -70);
+
+glEnd();
+
+glBegin(GL_POLYGON);
+glColor3f(0.5, 0.5, 0);
+glVertex2f(220, -70);
+glVertex2f(220, -84);
+glVertex2f(222, -84);
+glVertex2f(222, -70);
+glEnd();
+
+glBegin(GL_POLYGON);
+glColor3f(0.5, 0.5, 0);
+glVertex2f(272, -70);
+glVertex2f(272, -84);
+glVertex2f(274, -84);
+glVertex2f(274, -70);
+glEnd();
+
+glBegin(GL_POLYGON);
+glColor3f(0.5, 0.3, 0);
+glVertex2f(279, -70);
+glVertex2f(279, -84);
+glVertex2f(282, -84);
+glVertex2f(282, -70);
+
+glEnd();
+
+glBegin(GL_POLYGON);
+glColor3f(0.5, 0.5, 0);
+glVertex2f(206, -68);
+glVertex2f(206, -70);
+glVertex2f(288, -70);
+glVertex2f(288, -68);
+glEnd();
+
+//sitting space
+glBegin(GL_POLYGON);
+glColor3f(0.5, 0.3, 0);
+glVertex2f(215, -55);
+glVertex2f(206, -68);
+glVertex2f(288, -68);
+glVertex2f(280, -55);
+
+glEnd();
+
+//two vertical support pillers
+glColor3f(0.5, 0.5, 0);
+glBegin(GL_POLYGON);
+glVertex2f(220, -23);
+glVertex2f(220, -55);
+glVertex2f(222, -55);
+glVertex2f(222, -23);
+glEnd();
+
+glBegin(GL_POLYGON);
+glVertex2f(273, -23);
+glVertex2f(273, -55);
+glVertex2f(275, -55);
+glVertex2f(275, -23);
+glEnd();
+
+// outline of two vertical support pillers
+glColor3f(0, 0, 0);
+glLineWidth(1.5);
+glBegin(GL_LINES);
+glVertex2f(220, -23);
+glVertex2f(220, -55);
+
+glVertex2f(220, -55);
+glVertex2f(222, -55);
+
+glVertex2f(222, -55);
+glVertex2f(222, -23);
+
+glVertex2f(222, -23);
+glVertex2f(220, -23);
+glEnd();
+
+glBegin(GL_LINES);
+glVertex2f(273, -23);
+glVertex2f(273, -55);
+
+glVertex2f(273, -55);
+glVertex2f(275, -55);
+
+glVertex2f(275, -55);
+glVertex2f(275, -23);
+
+glVertex2f(275, -23);
+glVertex2f(273, -23);
+glEnd();
+
+//four horizontal support
+glColor3f(0.5, 0.3, 0);
+glBegin(GL_POLYGON);
+glVertex2f(215, -44);
+glVertex2f(215, -48);
+glVertex2f(280, -48);
+glVertex2f(280, -44);
+glEnd();
+
+glBegin(GL_POLYGON);
+glVertex2f(215, -38);
+glVertex2f(215, -42);
+glVertex2f(280, -42);
+glVertex2f(280, -38);
+glEnd();
+
+glBegin(GL_POLYGON);
+glVertex2f(215, -31.5);
+glVertex2f(215, -35.5);
+glVertex2f(280, -35.5);
+glVertex2f(280, -31.5);
+glEnd();
+
+glBegin(GL_POLYGON);
+glVertex2f(215, -25);
+glVertex2f(215, -29);
+glVertex2f(280, -29);
+glVertex2f(280, -25);
+glEnd();
+
+
+//outlines
+
+//LOWER SUPPORTS
+glColor3f(0, 0, 0);
+glLineWidth(1.5);
+glBegin(GL_LINES);
+glVertex2f(212, -70);
+glVertex2f(212, -84);
+
+glVertex2f(212, -84);
+glVertex2f(215, -84);
+
+glVertex2f(215, -84);
+glVertex2f(215, -70);
+
+glVertex2f(215, -70);
+glVertex2f(212, -70);
+
+glEnd();
+
+glBegin(GL_LINES);
+
+glVertex2f(220, -70);
+glVertex2f(220, -84);
+
+glVertex2f(220, -84);
+glVertex2f(222, -84);
+
+glVertex2f(222, -84);
+glVertex2f(222, -70);
+
+glVertex2f(222, -70);
+glVertex2f(220, -70);
+glEnd();
+
+glBegin(GL_LINES);
+glVertex2f(272, -70);
+glVertex2f(272, -84);
+
+glVertex2f(272, -84);
+glVertex2f(274, -84);
+
+glVertex2f(274, -84);
+glVertex2f(274, -70);
+
+glVertex2f(274, -70);
+glVertex2f(272, -70);
+glEnd();
+
+
+glBegin(GL_LINES);
+glVertex2f(279, -70);
+glVertex2f(279, -84);
+
+glVertex2f(279, -84);
+glVertex2f(282, -84);
+
+glVertex2f(282, -84);
+glVertex2f(282, -70);
+
+glVertex2f(282, -70);
+glVertex2f(279, -70);
+
+glEnd();
+
+
+
+//ABOVE THE LOWER SUPPORTS
+glBegin(GL_LINES);
+glVertex2f(206, -68);
+glVertex2f(206, -70);
+
+glVertex2f(206, -70);
+glVertex2f(288, -70);
+
+glVertex2f(288, -70);
+glVertex2f(288, -68);
+
+glVertex2f(288, -68);
+glVertex2f(206, -68);
+glEnd();
+
+//sitting space
+glBegin(GL_LINES);
+glVertex2f(215, -55);
+glVertex2f(206, -68);
+
+glVertex2f(206, -68);
+glVertex2f(288, -68);
+
+glVertex2f(288, -68);
+glVertex2f(280, -55);
+
+glVertex2f(280, -55);
+glVertex2f(215, -55);
+
+glEnd();
+
+
+
+
+
+//four horizontal support
+
+glBegin(GL_LINES);
+glVertex2f(215, -44);
+glVertex2f(215, -48);
+
+glVertex2f(215, -48);
+glVertex2f(280, -48);
+
+glVertex2f(280, -48);
+glVertex2f(280, -44);
+
+glVertex2f(280, -44);
+glVertex2f(215, -44);
+glEnd();
+
+glBegin(GL_LINES);
+glVertex2f(215, -38);
+glVertex2f(215, -42);
+
+glVertex2f(215, -42);
+glVertex2f(280, -42);
+
+glVertex2f(280, -42);
+glVertex2f(280, -38);
+
+glVertex2f(280, -38);
+glVertex2f(215, -38);
+glEnd();
+
+glBegin(GL_LINES);
+glVertex2f(215, -31.5);
+glVertex2f(215, -35.5);
+
+glVertex2f(215, -35.5);
+glVertex2f(280, -35.5);
+
+glVertex2f(280, -35.5);
+glVertex2f(280, -31.5);
+
+glVertex2f(280, -31.5);
+glVertex2f(215, -31.5);
+glEnd();
+
+glBegin(GL_LINES);
+glVertex2f(215, -25);
+glVertex2f(215, -29);
+
+glVertex2f(215, -29);
+glVertex2f(280, -29);
+
+glVertex2f(280, -29);
+glVertex2f(280, -25);
+
+glVertex2f(280, -25);
+glVertex2f(215, -25);
+glEnd();
+
+}
+
+
 
 void circle()
 {
@@ -507,10 +931,13 @@ glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
 glClear(GL_COLOR_BUFFER_BIT);
 
-
+soil_effect();
 tree();
 building();
 outlilne();
+lampPost();
+bench();
+
 
 glFlush(); // Render now
 
@@ -522,6 +949,8 @@ int main(int argc, char** argv) {
 
 glutInitWindowSize(1020, 700); // Set the window's initial width & height
 glutInit(&argc, argv); // Initialize GLUT
+glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH)-1020)/2,
+                       (glutGet(GLUT_SCREEN_HEIGHT)-700)/2);
 glutCreateWindow("OpenGL Setup"); // Create a window with the given title
 
 
